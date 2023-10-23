693721b71d01c9cf8a639facec57d59b98da0a54
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-23 01:36:02 -0300

    04-Setting up routes
    04-Setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 
 import Home from './components/home';
-import Post from './components/posts';
+import Posts from './components/posts';
 import Profile from './components/profile';
 
 
 const App = () => {
   return(
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <header>
+        Header
+      </header>
+      <Routes>
+        <Route path='posts' element={<Posts/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <footer>
+        footer
+      </footer>
+    </BrowserRouter>
   )
 }
 
