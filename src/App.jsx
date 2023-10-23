8ad7a824340aa00af4ca43dbcff09c589b53bdd8
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-23 01:28:34 -0300

    03-Creating components
    03-Creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+
+import Home from './components/home';
+import Post from './components/posts';
+import Profile from './components/profile';
+
+
 const App = () => {
   return(
     <>
